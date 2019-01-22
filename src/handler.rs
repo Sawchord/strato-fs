b42ad469136778e37baec28eb3f33e5931bb7081
Leon Tan <leon.arian.tan@gmail.com>
2019-01-22 22:39:07 +0100

    Adding Handler creation functions

--- src/handler.rs
+++ src/handler.rs
@@ -1,12 +1,14 @@
 use std::cmp::{PartialEq, Eq};
 use crate::{Directory, File};
 
+type FileImpl = Box<dyn File + Send + Sync>;
+type DirImpl = Box<dyn Directory + Send + Sync>;
 
 //FIXME: What are the visibility rules here?
 
 pub(crate) struct FileHandler {
     ino : u64,
-    handler: Box<dyn File + Send + Sync>
+    file_impl: FileImpl,
 }
 
 impl PartialEq for FileHandler {
@@ -20,19 +22,37 @@ impl Eq for FileHandler {}
 
 pub(crate) struct DirHandler {
     ino : u64,
-    handler: Box<dyn Directory + Send + Sync>
+    dir_impl: DirImpl,
 }
 
 impl PartialEq for DirHandler {
-    fn eq (&self, other : &DirHandler) -> bool { self.ino == other.ino
+    fn eq (&self, other : &DirHandler) -> bool {
+        self.ino == other.ino
     }
 }
 impl Eq for DirHandler {}
-
 
 
 #[derive(PartialEq, Eq)]
 pub(crate) enum Handler {
     File(FileHandler),
     Dir(DirHandler)
+}
+
+impl Handler {
+
+    pub(crate) fn new_file(file : FileImpl, ino : u64) -> Self {
+        Handler::File(FileHandler {
+            ino,
+            file_impl : file,
+        })
+    }
+
+    pub(crate) fn new_dir(dir : DirImpl, ino : u64) -> Self {
+        Handler::Dir(DirHandler {
+            ino,
+            dir_impl : dir,
+        })
+    }
+
 }
