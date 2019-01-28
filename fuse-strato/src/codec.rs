7a1adb034f1f21881f27b62fc4a61f7eefc964e2
Leon Tan <leon.arian.tan@gmail.com>
2019-01-28 23:05:21 +0100

    Implement tests for the fetches

--- fuse-strato/src/codec.rs
+++ fuse-strato/src/codec.rs
@@ -1,8 +1,8 @@
 use std::mem::size_of;
 use std::io::{Error, ErrorKind::*};
 use std::os::unix::ffi::OsStrExt;
-use std::path::Path;
-use std::ffi::OsStr;
+use std::path::PathBuf;
+use std::ffi::{OsStr, OsString};
 
 use std::fmt::Debug;
 
@@ -60,33 +60,38 @@ impl Decoder for FuseRequestDecoder {
 
 /// Helper functions
 // These functions are based on the work of Andreas Neuhaus under MIT license
-pub fn fetch<'a, T: Clone>(src: &mut BytesMut) -> T {
+pub fn fetch<T: Clone>(src: &mut BytesMut) -> T {
     let len = size_of::<T>();
     assert!(len <= src.len(), "out of data while fetching typed argument");
 
     // Return the data as the corresponding data type
-    let mut bytes = src.split_to(len);
+    let bytes = src.split_to(len);
 
     let dst: &T = unsafe {std::mem::transmute(bytes.as_ptr())};
     dst.clone()
 }
 
-//pub fn fetch_str<'a>(src: &mut BytesMut) -> &'a OsStr {
-//    let len = src.iter().position(|&c| c == 0)
-//        .expect("Ran out of data while parsing string");
-//
-//    let bytes = src.split_to(len+1);
-//    OsStr::new(str::from(&bytes))
-//}
-//
-//pub fn fetch_path<'a>(src: &mut BytesMut) -> &'a Path {
-//    Path::new(fetch_str(src))
-//}
+pub fn fetch_str(src: &mut BytesMut) -> OsString {
+    let len = src.iter().position(|&c| c == 0)
+        .expect("Ran out of data while parsing string");
+
+    let bytes = src.split_to(len);
+
+    // Discard null byte
+    src.advance(1);
+
+    OsString::from(std::str::from_utf8(&bytes).unwrap())
+}
+
+pub fn fetch_path(src: &mut BytesMut) -> PathBuf {
+    PathBuf::from(fetch_str(src))
+}
 
 #[cfg(test)]
 mod tests {
+
     #[test]
-    fn fetch_test1() {
+    fn fetch() {
         use super::*;
         use bytes::{BytesMut, BufMut};
 
@@ -106,9 +111,46 @@ mod tests {
             assert_eq!(buf.to_vec(), "TestTest".to_string().into_bytes());
             tmp
         };
+        // Let buf go out of scope to test, that the fetched value persists
 
         println!("{:x?}", ts);
         assert_eq!(ts, TestStruct { buff: 0x66667542, er: 0x20207265 });
 
     }
+
+    #[test]
+    fn fetch_str() {
+        use super::*;
+        use bytes::{BytesMut, BufMut};
+
+        let s = {
+            let mut buf = BytesMut::with_capacity(64);
+            buf.put("This is a test\0This is not fetched");
+
+            let s = fetch_str(&mut buf);
+            assert_eq!(buf.to_vec(), "This is not fetched".to_string().into_bytes());
+            s
+        };
+
+        assert_eq!(s, OsString::from("This is a test"));
+
+    }
+
+    #[test]
+    fn fetch_path() {
+        use super::*;
+        use bytes::{BytesMut, BufMut};
+
+        let p = {
+            let mut buf = BytesMut::with_capacity(64);
+            buf.put("/dev/fuse\0This is not fetched");
+
+            let p = fetch_path(&mut buf);
+            assert_eq!(buf.to_vec(), "This is not fetched".to_string().into_bytes());
+            p
+        };
+
+        assert_eq!(p, PathBuf::from("/dev/fuse"));
+    }
+
 }
